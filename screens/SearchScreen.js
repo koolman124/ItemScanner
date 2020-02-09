eb528efe6475a6ee910d0eaff742a9920707ac20
Jarman Liu <jarman124@gmail.com>
2020-02-09 16:10:15 -0500

    Initial commit

---
+++ screens/SearchScreen.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { Searchbar, Button } from 'react-native-paper';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class SearchScreen extends React.Component {
+  state = {
+    firstQuery: '',
+  };
+
+  render() {
+    const { firstQuery } = this.state;
+    return (
+        <View style={styles.container}>
+            <View style={styles.searchContent}>
+                <Searchbar
+                    style={styles.searchbar}
+                    placeholder="Search"
+                    onChangeText={query => { this.setState({ firstQuery: query }); }}
+                    value={firstQuery}
+                />
+
+                <Text>Or</Text> 
+
+                <Button style={styles.button} icon="camera" mode="outlined" onPress={() => console.log('Pressed')}>
+                    Scan
+                </Button>
+            </View>
+        </View>
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    search_container: {
+      flex: 1,
+      flexDirection: 'column'
+    },
+
+    searchbar: {
+        width: '95%',
+    },
+    searchContent: {
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    button: {
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 175
+    }
+  });
+  
