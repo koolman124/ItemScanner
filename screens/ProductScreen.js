07b199bbd14021becc1e8de7a00995020b2754f3
Jarman Liu <jarman124@gmail.com>
2020-04-13 17:05:13 -0400

    Add find near me button to product page

--- screens/ProductScreen.js
+++ screens/ProductScreen.js
@@ -1,5 +1,6 @@
 import * as WebBrowser from 'expo-web-browser';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Geocoder from 'react-native-geocoding';
 import {
   TouchableOpacity,
   Text,
@@ -20,9 +21,11 @@ export default function ProductScreen({ route, navigation }) {
   const [product_image, setProductImage] = useState(productImage);
   const [product_links, setProductLinks] = useState(productLinks);
   const [product_relatedItems, setRelatedItems] = useState(productRelatedItems);
+  const [position, setPosition] = useState({latitude: 0, longitude: 0});
+  const [error, setError] = useState("");
+  const [postal_code, setPostal] = useState("")
 
   function fetchItemSku(store, sku) {
-    // console.log("https://item-finder-app.herokuapp.com/api/v1/productinfo?store=".concat(store).concat("&sku=").concat(sku))
     return fetch("https://item-finder-app.herokuapp.com/api/v1/productinfo?store=".concat(store).concat("&sku=").concat(sku), {
       method: "GET",
       headers: {
@@ -44,6 +47,53 @@ export default function ProductScreen({ route, navigation }) {
       });
   }
 
+  function getStoresFromAPI(store, url, zip, { navigation }) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setPosition({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+        setError("");
+        Geocoder.from(position.coords.latitude, position.coords.longitude).then(json => {
+          var addressComponent = json.results[0].address_components;
+          //  console.log(addressComponent);
+          //  console.log(addressComponent[addressComponent.length - 1].short_name);
+           setPostal(addressComponent[addressComponent.length - 1].short_name);
+           var sku;
+          if (url.includes("target")){
+            var str = url.split("-")
+            sku = str[str.length-1]
+          }
+          if (url.includes("walmart")){
+            var str = url.split("/")
+            sku = str[str.length-1]
+          }
+          return fetch("https://item-finder-app.herokuapp.com/api/v1/productinfo/nearby?store=".concat(store).concat("&sku=").concat(sku).concat("&postal_code=").concat(zip), {
+            method: "GET",
+            headers: {
+              'Accept': 'application/json, text/plain, */*',  // It can be used to overcome cors errors
+              'Content-Type': 'application/json'
+            },
+          })
+            .then(response => response.json())
+            .then(responseJson => {
+              console.log(responseJson);
+              // navigation.navigate("Stores", {
+              //       stores: responseJson
+              // });
+            })
+            .catch(error => {
+              console.log("Error finding");
+              console.error(error);
+            });
+         }).catch(error => console.warn(error));
+      },
+      e => setError(e.message),
+      { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },
+    );
+  }
+
   return (
       <SafeAreaView style={{flex: 1}}>
         <View style={styles.imageContent}>
@@ -58,18 +108,23 @@ export default function ProductScreen({ route, navigation }) {
           <FlatList
               data = {product_links}
               renderItem={({item}) => 
+              <View>
                 <TouchableOpacity 
                   style={styles.buttonShape}
                   onPress={_ => handleBuyNowPress(item.link)}
                 >
                   <Text style={styles.textStyle}>{item.store}</Text>
                 </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.buttonShape}
+                  onPress={_ => getStoresFromAPI(item.store, item.link, postal_code, { navigation })}
+                >
+                  <Text style={styles.textStyle}>Find near me</Text>
+                </TouchableOpacity>
+              </View>
             }
             keyExtractor={(item, index) => index.toString()}
           />
-          <TouchableOpacity>
-            <Text>Find near me</Text>
-          </TouchableOpacity>
         </View>
         <Text style={styles.categoryText}>Related Items</Text>
         <FlatList
@@ -138,7 +193,7 @@ const styles = StyleSheet.create({
     borderRadius:10,
     borderWidth: 1,
     borderColor: '#fff',
-    width:250,
+    width:150,
     height: 45
   },
   textStyle: {
