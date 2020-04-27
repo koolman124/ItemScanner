d8a829c3576c6c7763f30c7dedee06d1266c3e72
AKandhai0311 <32681810+AKandhai0311@users.noreply.github.com>
2020-04-27 18:15:53 -0400

    Updated MedFilter Screen
    Got the checkbox input to reflect in firebase

--- screens/MedFilters.js
+++ screens/MedFilters.js
@@ -1,85 +1,70 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import CheckboxFormX from 'react-native-checkbox-form';
+import { CheckBox, Text, Button, StyleSheet, View } from 'react-native';
+import React, {Component} from 'react';
 import firebase from 'firebase'
 
-const allergies = [
-    {
-        label: 'Peanuts',
-        value: 'Peanuts',
-        checked: false
-    },
-    {
-        label: 'Chocolate',
-        value: 'Chocolate',
-        checked: false
-    },
-    {
-        label: 'Cinnamon',
-        value: 'Cinnamon',
-        checked: false
-    },
-];
-
-export default class MedFilters extends Component {
-    _onSelect = ( item ) => {
-      console.log(item);
-    };
-
-    constructor(props) {
-      super(props);
-      this.state = {
-        Peanuts:false,
-        Chocolate:false,
-        Cinnamon:false
-      };
+export default class MedFilters extends React.Component {
+  constructor(props) {
+    super(props)
+    // this is the default state on page load
+    this.state = {
+      Peanuts: false,
+      Chocolate: false,
+      Cinnamon: false,
     }
-    onFilter = () => {
-            firebase
-              .database()
-              .ref("users/" + firebase.auth().currentUser.uid + '/Filters')
-              .set({
-                Peanuts: this.state.Peanuts,
-                Chocolate: this.state.Chocolate,
-                Cinnamon: this.state.Cinnamon
-              });
-    };
+  }
+  onFilter = () => {
+    firebase
+      .database()
+      .ref("users/" + firebase.auth().currentUser.uid + '/Filters')
+      .set({
+        Peanuts: this.state.Peanuts,
+        Chocolate: this.state.Chocolate,
+        Cinnamon: this.state.Cinnamon
+      });
+};
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style= {styles.welcome}>Select all the filters that apply: </Text>
-              <CheckboxFormX
-                  style= {styles.form}
-                  dataSource={allergies}
-                  itemShowKey="label"
-                  itemCheckedKey="checked"
-                  iconSize={40}
-                  formHorizontal={false}
-                  labelHorizontal={true}
-                  onChecked={(item) => this._onSelect(item)}
-              />
-        <Button title="Submit" onPress={this.onFilter} />
-     </View>
-    );
+      <View>
+        <Text style = {styles.welcome}> Select all filters that apply:</Text>
+        <CheckBox
+          value={this.state.Peanuts}
+          onChange={() => this.setState({ Peanuts: !this.state.Peanuts })}
+        />
+        <Text style = {styles.label}> Peanuts </Text>
+
+        <CheckBox
+          value={this.state.Chocolate}
+          onChange={() => this.setState({ Chocolate: !this.state.Chocolate })}
+        />
+        <Text style = {styles.label}> Chocolate</Text>
+
+        <CheckBox
+          value={this.state.Cinnamon}
+          onChange={() => this.setState({ Cinnamon: !this.state.Cinnamon })}
+        />
+        <Text style = {styles.label}> Cinnamon</Text>
+        <Button title="Submit" onPress= {this.onFilter}/>
+      </View>
+    )
   }
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
     backgroundColor: '#F5FCFF',
-  },
-  form: {
-    flex: 1,
-    padding: 10,
-    fontSize: 20,
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
-})
+  label: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 20
+  }
+});
